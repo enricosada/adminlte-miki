094f67fe8f6294db5fad1de993b3162fef5e6a30
Enrico Sada <enrico.sada@walmart.com>
2020-04-26 18:11:37 +0200

    remove unused

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace w6.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
